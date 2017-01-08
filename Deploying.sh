95f35703590829fe33d28527ebfa58e48b1ad1a2
Nikeweke <Driver3@meta.ua>
2017-01-08 13:19:57 +0200

    Create Deploying.sh

---
+++ Deploying.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install nginx
+sudo apt-get install mysql-server
+sudo mysql_secure_installation
+sudo apt-get install php-fpm php-mysql php-mbstring
+sudo nano /etc/php/7.0/fpm/php.ini
